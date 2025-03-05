f7640d9611a76f403587246c2286582616ee9ed2
Rickard Hagelin <Hagelin.Rickard@gmail.com>
2025-03-05 18:36:57 +0000

    build with latest qmk

--- keyboards/preonic/keymaps/iamDrakkir/keymap.c
+++ keyboards/preonic/keymaps/iamDrakkir/keymap.c
@@ -36,11 +36,7 @@ enum preonic_keycodes {
   LOWER,
   RAISE,
   NUM,
-  COLEMAK_EN,
-  LOWER_EN,
-  RAISE_EN,
-  BACKLIT,
-  GLHF
+  BACKLIT
 };
 
 // Left-hand home row mods
@@ -65,17 +61,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise |  /   | Left | DOWN |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
   SE_GRV,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_6,    SE_7,    SE_8,    SE_9,    SE_0,    KC_BSPC,
   KC_TAB,  SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,    KC_DEL,
   KC_ESC,  SE_A,    SE_S,    SE_D,    SE_F,    SE_G,    SE_H,    SE_J,    SE_K,    SE_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,    SE_N,    SE_M,    SE_COMM, SE_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,    SE_N,    SE_M,    SE_COMM, SE_DOT,  KC_UP,   KC_ENT,
+  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Colemak_SE
@@ -88,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt |  NUM |  GUI  |Lower |    Space    |Raise |      | Left | Down |Right |
+ * | Ctrl | Alt |  NUM |  GUI  |Lower |    Space    |Raise |  /   | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK_SE] = LAYOUT_preonic_grid(
@@ -162,87 +158,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______,    SE_0, _______, _______, _______, _______ 
 ),
 
-/* Colemak_EN
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |      | Del  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |  "   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |  Up  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  |  NUM|  GUI  |Lower |    Space    |Raise |      | Left | Down |Right |
- * `-----------------------------------------------------------------------------------'
- */
-[_COLEMAK_EN] = LAYOUT_preonic_grid(
-  KC_GRV,     KC_1,   KC_2,    KC_3,     KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-  KC_TAB,     KC_Q,   KC_W,    KC_F,     KC_P,   KC_B,   KC_J,    KC_L,    KC_U,    KC_Y, KC_QUES,  KC_DEL,
-  KC_ESC,     KC_A, HOME_R,  HOME_S,   HOME_T,   KC_G,   KC_M,  HOME_N,  HOME_E,  HOME_I,    KC_O, KC_QUOT,
-  KC_LSFT,    KC_Z,   KC_X,    KC_C,     KC_D,   KC_V,   KC_K,    KC_H, KC_COMM,  KC_DOT,   KC_UP,  KC_ENT,
-  KC_LCTL, KC_LALT,    NUM, KC_LGUI, LOWER_EN, KC_SPC, KC_SPC,RAISE_EN, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
-),
-
-/* Lower_EN
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   "  |  @   |   !  |  +   |   \  |      |   <  |   _  |   ?  |      |   Å  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   ‘  |  &   |   *  |  `   |   >  |   -  |   ~  |  =   |   |  |   Ö  |   Ä  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   %  |      |  ^   |   $  |      |      |   #  |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_LOWER_EN] = LAYOUT_preonic_grid(
-  _______,    _______, _______, _______,   _______,   _______, _______,    _______,    _______,    _______, _______, _______,
-  _______, S(KC_QUOT), S(KC_2), S(KC_1), S(KC_EQL),   KC_BSLS, _______, S(KC_COMM), S(KC_MINS), S(KC_SLSH), _______, SE_ARNG,
-  _______,    KC_QUOT, S(KC_7), S(KC_8),    KC_GRV, S(KC_DOT), KC_MINS,  S(KC_GRV),     KC_EQL, S(KC_BSLS), SE_ODIA, SE_ADIA,
-  _______,    S(KC_5), _______, S(KC_6),   S(KC_4),   _______, _______,    S(KC_3),    _______,    _______, _______, _______,
-  _______,    _______, _______, _______,   _______,   _______, _______,    _______,    _______,    _______, _______, _______
-),
-
-/* Raise_EN
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   @  |      |      |   [  |   ]  |      |   UP |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |   (  |   )  | LEFT | DOWN |RIGHT | Play |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |   {  |   }  |      |      | Pg Up| Vol+ | Pg Dn|
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      | Prev | Vol- | Next |
- * `-----------------------------------------------------------------------------------'
- */
-[_RAISE_EN] = LAYOUT_preonic_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, S(KC_2), _______, _______, KC_LBRC, KC_RBRC, _______,   KC_UP, _______, _______, _______,
-  _______, _______, _______, _______, _______, S(KC_9), S(KC_0), KC_LEFT, KC_DOWN, KC_RIGHT, KC_MPLY, _______,
-  _______, _______, _______, _______, _______, S(KC_LBRC), S(KC_RBRC), _______, _______, KC_PGUP, KC_VOLU, KC_PGDN,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
-),
-
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |      |  Del |
+ * |      | Reset| Debug|      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|Qwerty|ColeSE|ColeEN|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      | GLHF |      |      |      |
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______,
-  _______,   RESET,   DEBUG, _______, _______, _______, _______, TERM_ON,   TERM_OFF,    _______, _______,  KC_DEL,
-  _______, _______,  MU_MOD,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP,  QWERTY, COLEMAK_SE, COLEMAK_EN, _______, _______,
-  _______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF,    GLHF,    _______,    _______, _______, _______,
+  _______, QK_BOOT, DB_TOGG, _______, _______, _______, _______, _______,    _______,    _______, _______,  KC_DEL,
+  _______, _______, MU_NEXT,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP,  QWERTY, COLEMAK_SE,    _______, _______, _______,
+  _______, AU_PREV, AU_NEXT,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, _______,    _______,    _______, _______, _______,
   BACKLIT, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
 )
 
@@ -259,12 +192,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case COLEMAK_SE:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_COLEMAK_SE);
-          }
-          return false;
-          break;
-        case COLEMAK_EN:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_COLEMAK_EN);
           }
           return false;
           break;
@@ -298,26 +225,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
-        case LOWER_EN:
-          if (record->event.pressed) {
-            layer_on(_LOWER_EN);
-            update_tri_layer(_LOWER_EN, _RAISE_EN, _ADJUST);
-          } else {
-            layer_off(_LOWER_EN);
-            update_tri_layer(_LOWER_EN, _RAISE_EN, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE_EN:
-          if (record->event.pressed) {
-            layer_on(_RAISE_EN);
-            update_tri_layer(_LOWER_EN, _RAISE_EN, _ADJUST);
-          } else {
-            layer_off(_RAISE_EN);
-            update_tri_layer(_LOWER_EN, _RAISE_EN, _ADJUST);
-          }
-          return false;
-          break;
         case BACKLIT:
           if (record->event.pressed) {
             register_code(KC_RSFT);
@@ -335,16 +242,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             #ifdef __AVR__
             writePinHigh(E6);
             #endif
-          }
-          return false;
-          break;
-        case GLHF:
-          if (record->event.pressed) {
-            SEND_STRING(SS_DOWN(X_LSFT));
-            SEND_STRING(SS_TAP(X_ENTER));
-            SEND_STRING(SS_UP(X_LSFT));
-            SEND_STRING("gl hfhfhfhfhf");
-            SEND_STRING(SS_TAP(X_ENTER));
           }
           return false;
           break;
